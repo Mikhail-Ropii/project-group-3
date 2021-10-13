3f8360498e953c6b48d602a64593ed23c1044b2b
OleksandrKutsevych <alexander18091993@gmail.com>
2021-10-13 16:49:04 +0300

    add bug fixes

--- src/js/anchor.js
+++ src/js/anchor.js
@@ -7,12 +7,27 @@
 		let anchor2Link  = d.getElementById('how-link');
 		let anchor3Link  = d.getElementById('our-link');
 		let anchor4Link = d.getElementById('contact-link');
+		let anchor5Link  = d.getElementById('about-link2');
+		let anchor6Link  = d.getElementById('how-link2');
+		let anchor7Link  = d.getElementById('our-link2');
+		let anchor8Link = d.getElementById('contact-link2');
+		let anchor9Link = d.getElementById('our-link3');
+		let anchor10Link  = d.getElementById('how-link3');
+
 	
 		//Anchors
 		let anchor1      = d.getElementById('about');
 		let anchor2      = d.getElementById('how');
 		let anchor3      = d.getElementById('our');
 		let anchor4      = d.getElementById('contact');
+		let anchor5      = d.getElementById('about');
+		let anchor6      = d.getElementById('how');
+		let anchor7      = d.getElementById('our');
+		let anchor8 = d.getElementById('contact');
+		let anchor9 = d.getElementById('our');
+		let anchor10 = d.getElementById('how');
+	
+
 		
 		anchor1Link.addEventListener('click', (e) => { scrollTo(anchor1, e) }, false);
 		anchor2Link.addEventListener('click', (e) => { scrollTo(anchor2, e) }, false);
@@ -21,14 +36,30 @@
 		// anchor2Link2.onclick = function() { scrollToSimple(document.documentElement, 0, 3000); }
 		anchor3Link.addEventListener('click', (e) => { scrollTo(anchor3, e) }, false);
 		
-		anchor4Link.addEventListener('click', (e) => { scrollTo(anchor4.offsetTop, e) }, false);
+		anchor4Link.addEventListener('click', (e) => { scrollTo(anchor4, e) }, false);
+		anchor5Link.addEventListener('click', (e) => { scrollTo(anchor5, e) }, false);
+		anchor6Link.addEventListener('click', (e) => { scrollTo(anchor6, e) }, false);
 		
+		// anchor2Link.onclick = function() { scrollToSimple(document.documentElement, 500, 3000); }
+		// anchor2Link2.onclick = function() { scrollToSimple(document.documentElement, 0, 3000); }
+		anchor7Link.addEventListener('click', (e) => { scrollTo(anchor7, e) }, false);
+		
+		anchor8Link.addEventListener('click', (e) => { scrollTo(anchor8, e) }, false);
+		anchor9Link.addEventListener('click', (e) => { scrollTo(anchor9, e) }, false);
+		anchor10Link.addEventListener('click', (e) => { scrollTo(anchor10.offsetTop, e) }, false);
 		
 		console.log(anchor2); //DEBUG
 		console.log('anchor1: '+scrollTopValue(anchor1)+' / '+offsetTopValue(anchor1)); //DEBUG
 		console.log('anchor2: '+scrollTopValue(anchor2)+' / '+offsetTopValue(anchor2)); //DEBUG
 		console.log('anchor3: '+scrollTopValue(anchor3)+' / '+offsetTopValue(anchor3)); //DEBUG
 		console.log('anchor4: '+scrollTopValue(anchor4)+' / '+offsetTopValue(anchor4)); //DEBUG
+		console.log('anchor5: '+scrollTopValue(anchor5)+' / '+offsetTopValue(anchor5)); //DEBUG
+		console.log('anchor6: '+scrollTopValue(anchor6)+' / '+offsetTopValue(anchor6)); //DEBUG
+		console.log('anchor7: '+scrollTopValue(anchor7)+' / '+offsetTopValue(anchor7)); //DEBUG
+		console.log('anchor8: '+scrollTopValue(anchor8)+' / '+offsetTopValue(anchor8)); //DEBUG
+		console.log('anchor9: '+scrollTopValue(anchor9)+' / '+offsetTopValue(anchor9)); //DEBUG
+		console.log('anchor10: '+scrollTopValue(anchor10)+' / '+offsetTopValue(anchor10)); //DEBUG
+
 		// d.addEventListener('scroll', (e) => { console.log(e) }, false); //DEBUG
 		
 		console.log('App loaded. Have fun!');
